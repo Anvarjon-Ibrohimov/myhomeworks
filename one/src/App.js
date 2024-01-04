077ad55b299a83779e3f4f4d109e427288e3086f
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-01-04 11:02:12 +0500

    counter

--- one/src/App.js
+++ one/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            value:0,
+        }
+    }
+        inc=()=>{
+        this.setState({
+                value:this.state.value+1,
+            })
+        }
+
+        dec=()=>{
+            if(this.state.value>0){
+                this.setState({
+                    value:this.state.value-1,
+                })
+            }
+            
+            }
+            
+        
+    
+    render(){
+        return  <div className="one">
+        <h1>Counter</h1>
+        <div className="counter">
+        <button class="btn btn-primary" onClick={this.dec}>-</button>
+        <h3>The Value is : {this.state.value}</h3>
+        <button class="btn btn-danger" onClick={this.inc}>+</button>
+        </div>
     </div>
-  );
+    }
 }
 
 export default App;
